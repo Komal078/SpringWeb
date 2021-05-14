98b63479bf27208adb062f99d3876c6689f8df6f
Komal078 <79501105+Komal078@users.noreply.github.com>
2021-05-14 19:23:48 +0530

    Add files via upload

---
+++ demo/src/main/java/com/Springboot/demo/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.Springboot.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+	@RequestMapping("/home")
+    public String hello(@RequestParam String name,ModelMap model)
+	{
+		model.put("name", name);
+		return "login";
+	}
+
+}
